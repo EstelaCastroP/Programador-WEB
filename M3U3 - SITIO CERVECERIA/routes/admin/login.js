fb04aa80fb1f8673956fcb771da49bded0d5021c
estela castro <castropignattae@gmail.com>
2023-07-27 22:22:53 -0300

    M3U3

--- M3U3 - SITIO CERVECERIA/routes/admin/login.js
+++ M3U3 - SITIO CERVECERIA/routes/admin/login.js
@@ -10,11 +10,15 @@ router.post('/', async (req, res, next) => {
     try {
         console.log(req.body);
         var usuario = req.body.usuario;
-        var password = req.body.password;
+        var password = req.body.password; 
 
-        var data = await usuariosModel.getUserAndPassword(usuario, password);
+        var data = await 
+
+ usuariosModel.getUserAndPassword(usuario, password);
 
         if (data != undefined) {
+             req.session.id_usuario = data.id;
+             req.session.nombre = data.usuario;
              res.redirect('/admin/novedades');
         } else {
             res.render('admin/login', {
